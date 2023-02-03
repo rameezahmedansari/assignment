be9247013034505bef37bfe27a287f449bc09cdc
Rameez Ahmed Ansari <90474804+rameezahmedansari@users.noreply.github.com>
2023-02-03 19:55:39 +0530

    3rd

--- index.js
+++ index.js
@@ -18,49 +18,59 @@ const Insuarance = localStorage.getItem("Insuarance Required");
 
 let formArray = [];
 
-function showAdd() {
-  document.querySelector(".formData").classList.remove("hidden");
-  document.querySelector(".formData").classList.add("show");
-  document.querySelector(".table").classList.add("hidden");
+let objStr = localStorage.getItem("formArray");
+if (objStr != null) {
+  // if data get from local storage is not null then parse the json file and save it to array
+  formArray = JSON.parse(objStr);
 }
 
-addBtn.onclick = () => {};
+function showAdd() {
+  document.querySelector(".formData").classList.remove("hidden");
+}
 
-function store(e) {
-  localStorage.setItem("First Name", fname.value);
-  localStorage.setItem("Last Name", lname.value);
-  localStorage.setItem("Dropdown", dropdown.value);
+function store() {
+  var genderVal;
+  var insureVal;
   for (i = 0; i < gender.length; i++) {
     if (gender[i].checked) {
-      localStorage.setItem("Gender", gender[i].value);
+      genderVal = gender[i].value;
     }
   }
-
   for (var i = 0; i < insuarance.length; i++) {
     if (insuarance[i].checked) {
-      localStorage.setItem("Insuarance Required", insuarance[i].value);
+      insureVal = insuarance[i].value;
     }
   }
+  let id = Math.round(Math.random() * 1000);
+  formArray.push({
+    id: id,
+    fname: fname.value,
+    lname: lname.value,
+    dropdown: dropdown.value,
+    gender: genderVal,
+    insuarance: insureVal,
+  });
+  localStorage.setItem("formArray", JSON.stringify(formArray));
+}
+
+function prevent(e) {
+  e.preventDefault();
 }
 
 function view() {
   document.querySelector(".formData").classList.add("hidden");
   document.querySelector(".table").classList.remove("hidden");
-  document.querySelector(".table").classList.add("show");
-  const FirstName = localStorage.getItem("First Name");
-  const LastName = localStorage.getItem("Last Name");
-  const Class = localStorage.getItem("Dropdown");
-  const Gender = localStorage.getItem("Gender");
-  const Insuarance = localStorage.getItem("Insuarance Required");
 
-  tableBody.innerHTML = `<tr>
-  <td>${FirstName}</td>
-  <td>${LastName}</td>
-  <td>${Gender}</td>
-  <td>${Class}</td>
-  <td>${Insuarance}</td>
- 
-</tr>`;
-
-  console.log(FirstName, LastName);
+  let statement = "";
+  formArray.forEach((user, i) => {
+    statement += `<tr>
+       <th scope="row">${i + 1}</th>
+       <td>${user.fname}</td>
+       <td>${user.lname}</td>
+       <td>${user.dropdown}</td>
+       <td>${user.gender}</td>
+       <td>${user.insuarance}</td>       
+   </tr>`;
+  });
+  tableBody.innerHTML = statement;
 }
